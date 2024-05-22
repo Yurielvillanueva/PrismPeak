170190618af2a7fd002e652e82173fa1c8a8681d
Tempest <107783416+Yurielvillanueva@users.noreply.github.com>
2024-05-22 14:38:57 +0800

    Delete ThePrismPeak directory

--- ThePrismPeak/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate a loading process
-    setTimeout(function() {
-        document.getElementById("loading-screen").style.display = "none";
-        document.getElementById("content").style.display = "block";
-    }, 40000); // 3 seconds for demonstration
-});
